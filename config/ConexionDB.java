ce948101e1b4b63feb72cd7aae76fc149e4a44c1
Carlos Angola <angolausmayo@gmail.com>
2025-11-13 20:49:50 -0400

    commit 2

--- config/ConexionDB.java
+++ config/ConexionDB.java
@@ -5,9 +5,12 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexionDB {
-    private String url = "jdbc:postgresql://localhost:5432/db_abastocruz";
+    /*private String url = "jdbc:postgresql://localhost:5432/db_abastocruz";
     private String usuario = "postgres";
-    private String pass = "";
+    private String pass = "";*/
+    private String url = "jdbc:postgresql://www.tecnoweb.org.bo/db_grupo18sa";
+    private String usuario = "grupo18sa";
+    private String pass = "grup018grup018*";
 
     public Connection getConnection() throws SQLException {
         return DriverManager.getConnection(url, usuario, pass);
